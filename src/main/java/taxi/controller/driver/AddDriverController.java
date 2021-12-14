9749c28a1bd585074d3d6376e719b7511af6d20c
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-12-14 17:42:55 +0200

    new files

---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,47 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+import taxi.service.PasswordValidationService;
+
+@WebServlet("/drivers/add")
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+    private final PasswordValidationService passwordValidationService
+            = (PasswordValidationService) injector.getInstance(PasswordValidationService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("license_number");
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        String repeatPassword = req.getParameter("repeat_password");
+        try {
+            passwordValidationService.validate(password, repeatPassword);
+            Driver driver = new Driver(name, licenseNumber, login, password);
+            driverService.create(driver);
+            resp.sendRedirect("/drivers/");
+        } catch (AuthenticationException e) {
+            req.setAttribute("errorMsg", e.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+        }
+    }
+}
